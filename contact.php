e7e6d7413c3f704cfad4751a1b340d2987f34583
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2021-08-18 09:01:57 +0530

    Create contact.php

--- contact.php
+++ contact.php
@@ -1,61 +1,16 @@
 <?php
-if(!isset($_GET['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
-$name = $_GET['name'];
-$visitor_email = $_GET['email'];
-$message = $_GET['message'];
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
 //Validate first
-if(empty($name)||empty($visitor_email)) 
+if(empty($name)||empty($email)) 
 {
     echo "Name and email are mandatory!";
-    exit;
 }
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
+else {
+    mail("yogeshkumarjamre1@gmail.com","HaCkErInDiA Message",$message);
 }
-
-$email_from = 'yogeshkumarjamre1@gmail.com';//<== update the email address
-$email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
-    
-$to = "yogeshkumarjamre1@gmail.com";//<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: thank-you.html');
-
-
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
-   
 ?> 
