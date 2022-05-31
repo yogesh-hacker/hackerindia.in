4d22c8bc65e9234de7bad1805887838e052d9e45
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2022-05-31 21:23:15 +0530

    Update main2.js

--- Movies/main2.js
+++ Movies/main2.js
@@ -1,12 +1,6 @@
 setTimeout(play,1500);
 
 function play(){
-    const iframevid = document.getElementById('moviedet').contentWindow.document.getElementById('moviename');
-    const name = iframevid.innerText;
-    const mydata = JSON.parse(JSON.stringify(data));
-    if(name === mydata[0].movieid){
-        const source = mydata[0].link;
-    }
     if (p2pml.hlsjs.Engine.isSupported()) {
         var engine = new p2pml.hlsjs.Engine();
         var player = videojs("moviesrc", {
@@ -17,6 +11,12 @@ function play(){
                 },
             },
         });
+        const iframevid = document.getElementById('moviedet').contentWindow.document.getElementById('moviename');
+        const name = iframevid.innerText;
+        const mydata = JSON.parse(JSON.stringify(data));
+        if(name === mydata[0].movieid){
+            const source = mydata[0].link;
+        }
         p2pml.hlsjs.initVideoJsContribHlsJsPlayer(player);
         player.src({
             src: source,
