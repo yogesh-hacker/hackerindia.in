d688f5dc90f0fa7004870d343b8a731e2b9ad0d1
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2022-06-02 08:38:21 +0530

    Add files via upload

---
+++ Movies/movies.js
@@ -0,0 +1,27 @@
+setTimeout(autodet,2000);
+
+function fetch(){
+    var iframevid=document.getElementById('srcfetch').contentWindow.document.getElementById('moviesrc');
+    var moviesrc=iframevid.getAttribute('src');
+    if(moviesrc==="#"){
+        document.getElementById('preposter').src="unavailable.jpg";
+        document.getElementById('moviename').innerHTML="Unavailable";
+        document.getElementById('moviename').style.color="red";
+    }
+}
+
+
+function autodet(){
+    const mydata = JSON.parse(JSON.stringify(data));
+    const movie = document.getElementById("moviename").innerText;
+    const poster = document.getElementById('preposter');
+    if(movie === mydata[0].movieid){
+        poster.src = mydata[0].poster;
+    }
+    if(movie === mydata[1].movieid){
+        poster.src = mydata[1].poster;
+    }
+    if(movie === mydata[2].movieid){
+        poster.src = mydata[2].poster;
+    }
+}
